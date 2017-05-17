00d4ae3886e69dd0dca9fec5b2a8c92293f33ddb
ajaxTheAllmighty <kolbaska205@gmail.com>
2017-05-17 15:42:36 +0600

    some shit done

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -25,26 +25,123 @@ var AppComponent = (function () {
     }
     AppComponent.prototype.getTarget = function () {
         var Target = event.target;
-        var id = ($((Target.parentNode).parentNode).attr('id'));
-        /*console.log('тык');
-        console.log(id);*/
+        var id = $(Target).attr('id');
         ID = parseInt(id);
     };
     AppComponent.prototype.saveChanges = function (id) {
         id = ID;
-        //console.log(id);
         $(event.target).attr('data-dismiss', 'modal'); //Закрыть модальное окно
-        $('#errorMessage').attr('style', 'visibility:false'); //сделать сообщение об ошибке невидимым
-        if (($('#nameInput').val() != '') && ($('#lNameInput').val() != '')) {
-            this.Users[id].name = $('#nameInput').val(); //сохранить новые значения
-            this.Users[id].lName = $('#lNameInput').val();
-            $('#nameInput').val(''); //очистить
-            $('#lNameInput').val(''); //инпуты
+        $('#editErrorMessage').attr('style', 'visibility:hidden'); //сделать сообщение об ошибке невидимым
+        if (($('#editNameInput').val() != '') && ($('#editLNameInput').val() != '')) {
+            this.Users[id].name = $('#editNameInput').val(); //сохранить новые значения
+            this.Users[id].lName = $('#editLNameInput').val();
+            $('#editNameInput').val(''); //очистить
+            $('#editLNameInput').val(''); //инпуты
+            this.drawTable();
         }
         else {
             $(event.target).removeAttr('data-dismiss'); // не  закрывать модальное окно
-            $('#errorMessage').removeAttr('style'); //сообщение снова видно
+            $('#editErrorMessage').removeAttr('style'); //сообщение снова видно
         }
+    };
+    AppComponent.prototype.deleteUser = function () {
+        this.Users.splice(ID, 1);
+        this.drawTable();
+    };
+    AppComponent.prototype.addUser = function () {
+        $(event.target).attr('data-dismiss', 'modal'); //Закрыть модальное окно
+        $('#addErrorMessage').attr('style', 'visibility:hidden'); //сделать сообщение об ошибке невидимым
+        if (($('#addNameInput').val() != '') && ($('#addLNameInput').val() != '')) {
+            var tempObj = { id: this.Users.length, name: $('#addNameInput').val(), lName: $('#addLNameInput').val() };
+            this.Users.push(tempObj);
+            $('#addNameInput').val('');
+            $('#addLNameInput').val('');
+            this.drawTable();
+        }
+        else {
+            $(event.target).removeAttr('data-dismiss'); // не  закрывать модальное окно
+            $('#addErrorMessage').removeAttr('style'); //сообщение снова видно
+        }
+    };
+    AppComponent.prototype.drawTable = function () {
+        console.log('drawing...');
+        var Table = $('table');
+        var tBody = $('tbody');
+        console.log(this.Users.length);
+        tBody.empty();
+        //элементы таблицы
+        /*for (let rowIndex = 0; rowIndex == this.Users.length - 1; rowIndex++) {
+            console.log(rowIndex);
+            console.log(this.Users[rowIndex]);
+            row.dataset.id = this.Users[rowIndex].id.toString(); // установить id строки
+                cell.innerHTML = this.Users[rowIndex].id.toString(); // текст внутри ячейки
+            row.appendChild(cell); // добавить ячейку в ряд
+                cell.innerHTML = this.Users[rowIndex].name;
+            row.appendChild(cell);
+                cell.innerHTML = this.Users[rowIndex].lName;
+            row.appendChild(cell);
+                cell.innerHTML = ''; // убрать текст из ячейки
+                    editButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки изменения
+                    editButton.setAttribute('data-toggle', 'modal');
+                    editButton.setAttribute('data-target', '#editModal');
+                    editButton.setAttribute('onclick','getTarget()');
+                    editButton.innerHTML = 'Изменить';
+                cell.appendChild(editButton); // добавить кнопку в ячейку
+            row.appendChild(cell);
+                    deleteButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки удаления
+                    deleteButton.setAttribute('onclick','deleteUser()');
+                    deleteButton.innerHTML = 'Удалить';
+                cell.appendChild(deleteButton);
+            tBody.appendChild(row); //добавить строку в таблицу
+        };*/
+        for (var _i = 0, _a = this.Users; _i < _a.length; _i++) {
+            var item = _a[_i];
+            console.log('start');
+            var row = document.createElement('tr');
+            var cells = [];
+            var editButton = document.createElement('button');
+            var deleteButton = document.createElement('button');
+            row.setAttribute('id', item.id.toString()); // установить id строки
+            var cell1 = document.createElement('th');
+            cell1.innerHTML = item.toString(); // текст внутри ячейки
+            cells.push(cell1);
+            console.log(cells);
+            var cell2 = document.createElement('th');
+            cell2.innerHTML = item.name;
+            cells.push(cell2);
+            console.log(cells);
+            var cell3 = document.createElement('th');
+            cell3.innerHTML = item.lName;
+            cells.push(cell3);
+            console.log(cells);
+            editButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки изменения
+            editButton.setAttribute('data-toggle', 'modal');
+            editButton.setAttribute('data-target', '#editModal');
+            editButton.innerHTML = 'Изменить';
+            var cell4 = document.createElement('th');
+            cell4.appendChild(editButton);
+            cells.push(cell4);
+            console.log(cells);
+            deleteButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки удаления
+            deleteButton.addEventListener('click', this.deleteUser, false);
+            deleteButton.innerHTML = 'Удалить';
+            var cell5 = document.createElement('th');
+            cell5.appendChild(deleteButton);
+            cells.push(cell5);
+            console.log(cells);
+            for (var _b = 0, cells_1 = cells; _b < cells_1.length; _b++) {
+                var tempCell = cells_1[_b];
+                console.log('appending');
+                row.appendChild(tempCell);
+            }
+            row.addEventListener('click', this.getTarget, false);
+            tBody.append(row); //добавить строку в таблицу
+        }
+        Table.append(tBody); // добавить тело к таблице
+        console.log('drawing done');
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.drawTable();
     };
     return AppComponent;
 }());
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 var ID:number;
 
@@ -7,6 +7,7 @@ export class entity {
     name:string;
     lName:string;
 }
+
 
 let USERS: entity[] = [
     {id: 0, name: 'Jon Smith', lName: 'Last Name'},
@@ -18,32 +19,128 @@ let USERS: entity[] = [
     templateUrl: './template.html',
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
     name = 'Angular';
     Users = USERS;
 
     getTarget() {
         let Target = event.target;
-        let id = ($(((<HTMLButtonElement>Target).parentNode).parentNode).attr('id'));
-        /*console.log('тык');
-        console.log(id);*/
+        let id = $(<HTMLTableRowElement>Target).attr('id');
         ID = parseInt(id);
     }
     saveChanges(id:number) {
         id = ID;
-        //console.log(id);
         $(<HTMLButtonElement>event.target).attr('data-dismiss', 'modal'); //Закрыть модальное окно
-        $('#errorMessage').attr('style','visibility:false');//сделать сообщение об ошибке невидимым
-            if (($('#nameInput').val() != '' ) && ($('#lNameInput').val()!='')) {
-                this.Users[id].name = $('#nameInput').val();//сохранить новые значения
-                this.Users[id].lName = $('#lNameInput').val();
-                $('#nameInput').val('');//очистить
-                $('#lNameInput').val('');//инпуты
+        $('#editErrorMessage').attr('style','visibility:hidden');//сделать сообщение об ошибке невидимым
+            if (($('#editNameInput').val() != '' ) && ($('#editLNameInput').val()!='')) {
+                this.Users[id].name = $('#editNameInput').val();//сохранить новые значения
+                this.Users[id].lName = $('#editLNameInput').val();
+                $('#editNameInput').val('');//очистить
+                $('#editLNameInput').val('');//инпуты
+                this.drawTable();
             }
             else {
                 $(<HTMLButtonElement>event.target).removeAttr('data-dismiss');// не  закрывать модальное окно
-                $('#errorMessage').removeAttr('style');//сообщение снова видно
+                $('#editErrorMessage').removeAttr('style');//сообщение снова видно
             }
+    }
+    deleteUser () {
+        this.Users.splice(ID,1);
+        this.drawTable();
+    }
+    addUser() {
+        $(<HTMLButtonElement>event.target).attr('data-dismiss', 'modal'); //Закрыть модальное окно
+        $('#addErrorMessage').attr('style','visibility:hidden');//сделать сообщение об ошибке невидимым
+        if (($('#addNameInput').val() != '' ) && ($('#addLNameInput').val() != '' ))
+        {
+            let tempObj:entity = {id: this.Users.length, name: $('#addNameInput').val(), lName: $('#addLNameInput').val()};
+            this.Users.push(tempObj);
+            $('#addNameInput').val('');
+            $('#addLNameInput').val('');
+            this.drawTable();
+        }
+        else {
+            $(<HTMLButtonElement>event.target).removeAttr('data-dismiss');// не  закрывать модальное окно
+            $('#addErrorMessage').removeAttr('style');//сообщение снова видно
+        }
+    }
+    drawTable() {
+        console.log('drawing...');
+        let Table = $('table');
+        let tBody = $('tbody');
+        console.log(this.Users.length);
+        tBody.empty();
+        //элементы таблицы
+            /*for (let rowIndex = 0; rowIndex == this.Users.length - 1; rowIndex++) {
+                console.log(rowIndex);
+                console.log(this.Users[rowIndex]);
+                row.dataset.id = this.Users[rowIndex].id.toString(); // установить id строки
+                    cell.innerHTML = this.Users[rowIndex].id.toString(); // текст внутри ячейки
+                row.appendChild(cell); // добавить ячейку в ряд
+                    cell.innerHTML = this.Users[rowIndex].name;
+                row.appendChild(cell);
+                    cell.innerHTML = this.Users[rowIndex].lName;
+                row.appendChild(cell);
+                    cell.innerHTML = ''; // убрать текст из ячейки
+                        editButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки изменения
+                        editButton.setAttribute('data-toggle', 'modal');
+                        editButton.setAttribute('data-target', '#editModal');
+                        editButton.setAttribute('onclick','getTarget()');
+                        editButton.innerHTML = 'Изменить';
+                    cell.appendChild(editButton); // добавить кнопку в ячейку
+                row.appendChild(cell);
+                        deleteButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки удаления
+                        deleteButton.setAttribute('onclick','deleteUser()');
+                        deleteButton.innerHTML = 'Удалить';
+                    cell.appendChild(deleteButton);
+                tBody.appendChild(row); //добавить строку в таблицу
+            };*/
+            for (let item of this.Users){
+                console.log('start');
+                let row = document.createElement('tr');
+                let cells = [];
+                let editButton = document.createElement('button');
+                let deleteButton = document.createElement('button');
+                    row.setAttribute('id',item.id.toString()) // установить id строки
+                let cell1 = document.createElement('th');
+                    cell1.innerHTML = item.toString(); // текст внутри ячейки
+                    cells.push(cell1);
+                console.log(cells);
+                let cell2 = document.createElement('th');
+                    cell2.innerHTML = item.name;
+                    cells.push(cell2);
+                console.log(cells);
+                let cell3 = document.createElement('th');
+                    cell3.innerHTML = item.lName;
+                    cells.push(cell3);
+                console.log(cells);
+                    editButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки изменения
+                    editButton.setAttribute('data-toggle', 'modal');
+                    editButton.setAttribute('data-target', '#editModal');
+                    editButton.innerHTML = 'Изменить';
+                let cell4 = document.createElement('th');
+                    cell4.appendChild(editButton);
+                    cells.push(cell4);
+                console.log(cells);
+                    deleteButton.className = 'crud__DeleteEditButton form-control'; //настройка кнопки удаления
+                    deleteButton.addEventListener('click',this.deleteUser,false)
+                    deleteButton.innerHTML = 'Удалить';
+                let cell5 = document.createElement('th');
+                    cell5.appendChild(deleteButton);
+                    cells.push(cell5);
+                console.log(cells);
+                    for (let tempCell of cells){
+                         console.log('appending');
+                         row.appendChild(tempCell);
+                     }
+                    row.addEventListener('click',this.getTarget,false)
+                    tBody.append(row); //добавить строку в таблицу
+            }
+            Table.append(tBody); // добавить тело к таблице
+            console.log('drawing done');
+    }
+    ngOnInit ():void {
+        this.drawTable();
     }
 }
 //TODO сделать удаление и динамическую отрисовку таблицы
